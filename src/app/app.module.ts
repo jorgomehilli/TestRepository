41c5a7f491999bd3cda54bedf2e50f59c13727f1
jorgomehilli <jorgo.mehilli@gmail.com>
2019-11-01 10:30:50 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import{ ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component'
-import{FormsModule} from '@angular/forms'
+import{ FormsModule } from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
